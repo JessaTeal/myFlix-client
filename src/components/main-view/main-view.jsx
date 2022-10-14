b6db595fddbb9dbdd004dbb58e04ebab7c956ecd
JessaTeal <jessateal@gmail.com>
2022-10-14 12:21:00 -0500

    update array

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+
 import {MovieCard} from '../movie-card/movie-card';
 import {MovieView} from '../movie-view/movie-view';
 
@@ -8,13 +10,21 @@ export class MainView extends React.Component {
     constructor(){
         super();
         this.state = {
-            movies: [
-                {_id: 1, Title: 'Gladiator', Description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.', ImagePath: 'https://flxt.tmsimg.com/assets/p24674_p_v8_ae.jpg'},
-                {_id: 2, Title: 'The Shawshank Redemption', Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', ImagePath: 'https://m.media-amazon.com/images/I/519NBNHX5BL._AC_SY580_.jpg'},
-                {_id: 3, Title: 'Inception', Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.', ImagePath: 'https://m.media-amazon.com/images/M/MV5BMjExMjkwNTQ0Nl5BMl5BanBnXkFtZTcwNTY0OTk1Mw@@._V1_.jpg'},
-            ],
+            movies: [],
             selectedMovie: null
         };
+    }
+
+    componentDidMount(){
+        axios.get('https://jessaflix.herokuapp.com/movies')
+        .then(response => {
+            this.setState({
+                movies: response.data
+            });
+        })
+        .catch(error => {
+            console.log(error)
+        });
     }
 
     setSelectedMovie(newSelectedMovie) {
@@ -26,7 +36,7 @@ export class MainView extends React.Component {
     render() {
         const { movies, selectedMovie } = this.state;
 
-        if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+        if (movies.length === 0) return <div className="main-view" />;
 
         return (
             <div className="main-view">
