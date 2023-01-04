9347224c66eb9a8083996748d7e6d073580b680e
JessaTeal <jessateal@gmail.com>
2023-01-04 13:23:08 -0600

    kkupdate

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,10 +1,10 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './login-view.scss';
 import PropTypes from 'prop-types';
 
 export function LoginView(props) {
-    const[ username, setUsername ] = useState('');
-    const[ password, setPassword ] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -15,14 +15,14 @@ export function LoginView(props) {
         <form className="login">
             <label className="username">
                 Username:
-                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} required />
             </label>
             <label>
                 Password:
-                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            </label>                
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+            </label>
             <button type='button' onClick={handleSubmit}>Submit</button>
             <button type='button' onClick={handleSubmit}>New User!</button>
-            </form>
-        );
-    }
+        </form>
+    );
+}
